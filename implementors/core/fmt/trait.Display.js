6f16005aa4d81f19d4cf5a26716b369b03ed7969
Travis CI User <travis@example.org>
2017-09-10 10:04:29 +0000

    Documentation for mlalic/solicit@fdccb36

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["solicit"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"solicit/http/client/tls/enum.TlsConnectError.html\" title=\"enum solicit::http::client::tls::TlsConnectError\">TlsConnectError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"solicit/http/client/struct.CleartextConnectError.html\" title=\"struct solicit::http::client::CleartextConnectError\">CleartextConnectError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"solicit/http/struct.ConnectionError.html\" title=\"struct solicit::http::ConnectionError\">ConnectionError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"solicit/http/enum.HttpError.html\" title=\"enum solicit::http::HttpError\">HttpError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/iter/iterator/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["solicit"] = ["impl&lt;'a, S&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"solicit/http/session/struct.StreamIter.html\" title=\"struct solicit::http::session::StreamIter\">StreamIter</a>&lt;'a, S&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;S: <a class=\"trait\" href=\"solicit/http/session/trait.Stream.html\" title=\"trait solicit::http::session::Stream\">Stream</a> + 'a,&nbsp;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
